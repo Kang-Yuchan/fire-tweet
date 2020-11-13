5746d5bd40deb04ddb75d053c38261658b1a2152
Kang-Yuchan <mikuplayer123@gmail.com>
2020-11-13 22:48:54 +0900

    log out

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { auth } from '../firebase';
 
-const Home = () => (
-  <div>Home</div>
-)
+const Home = () => {
+  const logout = () => {
+    auth.signOut();
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={logout}>log out</button>
+    </div>
+  )
+};
 
 export default Home;
