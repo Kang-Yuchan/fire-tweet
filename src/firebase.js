7f930a50c4f10423afded2e0212d54f6530c0b5e
Kang-Yuchan <mikuplayer123@gmail.com>
2020-11-13 11:18:18 +0900

    hide firebase config in .env

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAGPADaADRYI1KT7HFhINT5q50bRczL_HE",
-  authDomain: "fire-tweet-7c336.firebaseapp.com",
-  databaseURL: "https://fire-tweet-7c336.firebaseio.com",
-  projectId: "fire-tweet-7c336",
-  storageBucket: "fire-tweet-7c336.appspot.com",
-  messagingSenderId: "909410632689",
-  appId: "1:909410632689:web:ff86bbc858b428f974fa50"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DB_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 export default firebase.initializeApp(firebaseConfig);
