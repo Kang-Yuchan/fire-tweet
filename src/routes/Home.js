880d6a09230c2c69d1aa9408c90c3faf16668ce6
Kang-Yuchan <mikuplayer123@gmail.com>
2020-11-14 15:19:13 +0900

    getting tweets data

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { db } from '../firebase';
 
 const Home = () => {
   const [tweet, setTweet] = useState("");
+  const [tweets, setTweets] = useState([]);
 
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -17,12 +18,33 @@ const Home = () => {
     const value = e.target.value;
     setTweet(value);
   }
+
+  const getDbTweets = async () => {
+    const dbTweets =  await db.collection("tweets").get(); 
+    dbTweets.forEach((document) => {
+      const tweetDataObject = {
+        id: document.id,
+        ...document.data(),
+      };
+      setTweets((prev) => [tweetDataObject, ...prev]);
+    });
+  };
+
+  useEffect(() => {
+    getDbTweets();
+  }, [])
+
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input type="text" placeholder="Let's tweet!" maxLength={120} onChange={onChange} value={tweet} />
         <button type="submit">tweet</button>
       </form>
+      {tweets.map((tweetData) => (
+        <div key={tweetData.id}>
+          <span>{tweetData.tweet}</span>
+        </div>
+      ))}
     </div>
   )
 };
