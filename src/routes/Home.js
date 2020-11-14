56723ed7b53f73d259888d408a1d0cd1e62ee162
Kang-Yuchan <mikuplayer123@gmail.com>
2020-11-14 12:12:24 +0900

    Navigation & log out

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import { auth } from '../firebase';
 
 const Home = () => {
-  const logout = () => {
-    auth.signOut();
-  };
-
   return (
     <div>
-      <button type="button" onClick={logout}>log out</button>
+      Home
     </div>
   )
 };
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { auth } from '../firebase';
 
-const Profile = () => (
-  <div>Profile</div>
-)
+const Profile = () => {
+  const history = useHistory();
+  const logout = () => {
+    auth.signOut();
+    history.push('/');
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={logout}>log out</button>
+    </div>
+  )
+};
 
 export default Profile;
